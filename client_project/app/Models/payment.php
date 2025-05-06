8fc70e5eaf7762ff5120533dd9656eb137a39e33
Nazarzu <140492595+Nazarzu@users.noreply.github.com>
2025-05-06 19:15:06 +0700

    Delete client_project directory

--- client_project/app/Models/payment.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace App\Models;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-class payment extends Model
-{
-    use HasFactory;
-    protected $fillable = ['invoice_id', 'amount', 'payment_date', 'payment_method'];
-}
--- client_project/resources/views/livewire/chat/chat.blade.php
+++
@@ -1,8 +0,0 @@
-<div class=" fixed  h-full  flex bg-white border dark:bg-[#161A1D] dark:border-[#22272B]  lg:shadow-sm overflow-hidden inset-0 lg:top-16 top-36  lg:inset-x-2 m-auto lg:h-[90%] rounded-t-lg container">
-    <div class="hidden lg:flex relative w-full md:w-[320px] xl:w-[400px] overflow-y-auto shrink-0 h-full border dark:border-[#22272B]">
-        <livewire:chat.chat-list :selectedConversation="$selectedConversation" :query="$query">
-    </div>
-    <div class="grid   w-full border-l h-full relative overflow-y-auto dark:border-[#22272B]" style="contain:content">
-      <livewire:chat.chat-box :selectedConversation="$selectedConversation">
-    </div>
-</div>
